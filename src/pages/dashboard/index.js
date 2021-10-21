f49bc856e182408a17cde1d440d5cb42926cdf6f
aline <alinne.1904@gmail.com>
2021-10-20 21:56:26 -0300

    initial commit

---
+++ src/pages/dashboard/index.js
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import UserCard from "../../components/User/Card/Card";
+import UserList from "../../components/User/List/List";
+import RepositoryList from "../../components/Repository/List/List";
+import FollowersQ from "./graphql/FollowersQ";
+import FollowingQ from "./graphql/FollowingQ";
+import { useQuery } from "@apollo/client";
+import "./dashboard.css";
+
+export default function PagesDashboard() {
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [username] = useState(
+    () => window.localStorage.getItem("github_username") || ""
+  );
+
+  const { data: followers, error: followerError } = useQuery(FollowersQ, {
+    variables: {
+      username,
+    },
+  });
+
+  const { data: following, error: followingError } = useQuery(FollowingQ, {
+    variables: {
+      username,
+    },
+  });
+
+  const error = followerError || followingError;
+
+  return (
+    <div>
+      <header className="PagesDashboard__topbar">{username}</header>
+      {error ? (
+        <div>Algo de errado</div>
+      ) : (
+        <section className="PagesDashboard__content">
+          <UserList title="Followers">
+            {followers?.user.followers.nodes.map((follower) => (
+              <UserCard
+                key={follower.id}
+                user={follower}
+                isSelected={selectedUser === follower.login}
+                setSelectedUser={setSelectedUser}
+                onClick={() => setSelectedUser(follower.login)}
+              />
+            ))}
+          </UserList>
+          <UserList title="Following">
+            {following?.user.following.nodes.map((following) => (
+              <UserCard
+                key={following.id}
+                user={following}
+                isSelected={selectedUser === following.login}
+                onClick={() => setSelectedUser(following.login)}
+              />
+            ))}
+          </UserList>
+          <RepositoryList username={selectedUser} />
+        </section>
+      )}
+    </div>
+  );
+}
---
+++ src/pages/root.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
+import PagesLogin from "./login";
+import PagesDashboard from "./dashboard";
+import PagesAuth from "./auth";
+
+export default function PagesRoot() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/auth">
+          <PagesAuth />
+        </Route>
+        <Route path="/login">
+          <PagesLogin />
+        </Route>
+        <Route path="/dashboard">
+          <PagesDashboard />
+        </Route>
+        <Route>
+          <Redirect from="/" to="/login" />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
