522bcb5bd42f5240d84affd509def2d7ed200a93
aline <alinne.1904@gmail.com>
2021-10-24 00:08:37 -0300

    feat: add loading, adjust graphql and listing

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,31 +1,76 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import UserCard from "../../components/User/Card/Card";
 import UserList from "../../components/User/List/List";
 import RepositoryList from "../../components/Repository/List/List";
+import RepositoryCard from "../../components/Repository/Card/Card";
+import IssuesCard from "../../components/Issues/Card/Card";
+import IssuesList from "../../components/Issues/List/List";
 import FollowersQ from "./graphql/FollowersQ";
 import FollowingQ from "./graphql/FollowingQ";
+import RepositoryQ from "./graphql/RepositoryQ";
 import { useQuery } from "@apollo/client";
 import "./dashboard.css";
 
 export default function PagesDashboard() {
   const [selectedUser, setSelectedUser] = useState(null);
+  const [selectedRepo, setSelectedRepo] = useState(null);
+  const [selectedIssuesByRepo, setSelectedIssuesByRepo] = useState(null);
+
   const [username] = useState(
     () => window.localStorage.getItem("github_username") || ""
   );
 
-  const { data: followers, error: followerError } = useQuery(FollowersQ, {
+  const {
+    data: followers,
+    error: followerError,
+    loading: followerLoading,
+  } = useQuery(FollowersQ, {
     variables: {
       username,
     },
   });
 
-  const { data: following, error: followingError } = useQuery(FollowingQ, {
+  const {
+    data: following,
+    error: followingError,
+    loading: followingLoading,
+  } = useQuery(FollowingQ, {
     variables: {
       username,
     },
   });
 
-  const error = followerError || followingError;
+  const {
+    data: repositories,
+    error: repositoriesError,
+    refetch,
+    loading: RepositoriesLoading
+  } = useQuery(RepositoryQ, {
+    variables: {
+      username: selectedUser || username,
+    },
+  });
+
+  const error = followerError || followingError || repositoriesError;
+
+  useEffect(() => {
+    selectedUser && refetch();
+  }, [selectedUser]);
+
+  useEffect(() => {
+    selectedRepo && refetch();
+  }, [selectedRepo]);
+
+  const handleSelectedUser = (user) => {
+    setSelectedUser(user.login);
+    setSelectedRepo(null);
+    setSelectedIssuesByRepo([]);
+  };
+
+  const handleSelectedRepo = (repo) => {
+    setSelectedRepo(repo.name);
+    setSelectedIssuesByRepo(repo?.issues?.nodes);
+  };
 
   return (
     <div>
@@ -35,27 +80,48 @@ export default function PagesDashboard() {
       ) : (
         <section className="PagesDashboard__content">
           <UserList title="Followers">
-            {followers?.user.followers.nodes.map((follower) => (
-              <UserCard
-                key={follower.id}
-                user={follower}
-                isSelected={selectedUser === follower.login}
-                setSelectedUser={setSelectedUser}
-                onClick={() => setSelectedUser(follower.login)}
-              />
-            ))}
+            {followerLoading 
+              ? "Loading..."
+              : followers?.user.followers.nodes.map((follower) => (
+                  <UserCard
+                    key={follower.id}
+                    user={follower}
+                    isSelected={selectedUser === follower.login}
+                    onClick={() => handleSelectedUser(follower)}
+                  />
+                ))}
           </UserList>
           <UserList title="Following">
-            {following?.user.following.nodes.map((following) => (
-              <UserCard
-                key={following.id}
-                user={following}
-                isSelected={selectedUser === following.login}
-                onClick={() => setSelectedUser(following.login)}
+            {followingLoading 
+              ? "Loading..."
+              : following?.user.following.nodes.map((following) => (
+                  <UserCard
+                    key={following.id}
+                    user={following}
+                    isSelected={selectedUser === following.login}
+                    onClick={() => handleSelectedUser(following)}
+                  />
+                ))}
+          </UserList>
+          <RepositoryList title="Repositories">
+            {RepositoriesLoading
+              ? "Loading..."
+              : repositories?.user.repositories.nodes.map((repository) => (
+              <RepositoryCard
+                key={repository.id}
+                repo={repository}
+                isSelected={selectedRepo === repository.name}
+                onClick={() => handleSelectedRepo(repository)}
               />
             ))}
-          </UserList>
-          <RepositoryList username={selectedUser} />
+          </RepositoryList>
+          {selectedIssuesByRepo?.length > 0 && (
+            <IssuesList title="Issues">
+              {selectedIssuesByRepo?.map((issue) => (
+                <IssuesCard key={issue.id} issues={issue} />
+              ))}
+            </IssuesList>
+          )}
         </section>
       )}
     </div>
