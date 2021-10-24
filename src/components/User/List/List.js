4d276e5ad0e75c6ef8e7265cb1fff3419c15354b
aline <alinne.1904@gmail.com>
2021-10-24 00:06:33 -0300

    style: change of style in userList

--- src/components/User/List/List.js
+++ src/components/User/List/List.js
@@ -2,14 +2,12 @@ import SimpleBar from "simplebar-react";
 import "./List.css";
 
 export default function UserList({ title, children }) {
-
   return (
     <div className="UserList">
       <h3>
         {title}
-        {/* {loading && <span>Loading...</span>} */}
       </h3>
-      <SimpleBar style={{ maxHeight: 500 }}>
+      <SimpleBar style={{ maxHeight: "calc(100vh - 11rem)" }}>
         <ul className="UserList__content">{children}</ul>
       </SimpleBar>
     </div>
