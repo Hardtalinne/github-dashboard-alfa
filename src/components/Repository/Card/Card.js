b2e0cf8bd21cb495d3b30b1e17fca9a97956a4f0
aline <alinne.1904@gmail.com>
2021-10-24 00:06:02 -0300

    fix: fix attribute listing

--- src/components/Repository/Card/Card.js
+++ src/components/Repository/Card/Card.js
@@ -1,19 +1,37 @@
-import { GoStar, GoRepoForked } from "react-icons/go";
+import { GoStar, GoRepoForked, GoIssueOpened } from "react-icons/go";
 import "./Card.css";
 
-export default function RepositoryCard({ repo }) {
+export default function RepositoryCard({ repo, isSelected, onClick }) {
   return (
-    <div className="RepositoryCard">
-      <h3>{repo.name}</h3>
-      <div className="RepositoryCard__additional-info">
-        <span>
-          <GoStar /> {repo.stargazers_count || 0}
-        </span>
-        <span>
-          <GoRepoForked /> {repo.forks_count || 0}
-        </span>
-        {repo.language && <span>{repo.language}</span>}
+    <li
+      onClick={onClick}
+      className={`RepositoryCard ${isSelected && "RepositoryCard--selected"}`}
+    >
+      <div>
+        <h3>{repo.name}</h3>
+        <div className="RepositoryCard__additional-info">
+          <span>
+            <GoStar /> {repo.stargazerCount || 0}
+          </span>
+          <span>
+            <GoRepoForked /> {repo.forkCount || 0}
+          </span>
+          <span>
+            <GoIssueOpened /> {repo.issues.nodes.length || 0}
+          </span>
+        </div>
+        <div className="RepositoryCard__languages">
+          {repo.languages.nodes.length > 0 &&
+            repo.languages.nodes.map((language) => (
+              <p>
+                {" "}
+                {repo.languages.nodes.length > 1
+                  ? language.name + " |"
+                  : language.name}
+              </p>
+            ))}
+        </div>
       </div>
-    </div>
+    </li>
   );
 }
